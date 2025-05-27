069aabca4732c5ce78237c72ccba6ba47c9fd082
DESKTOP-DHEBCDJ\andre <andreslopez>
2025-05-27 17:24:51 -0500

    Ajuste de server en vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,8 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // Esto permite acceso desde la red local
-    port: 5173, // o el puerto que uses
-  },
 });
